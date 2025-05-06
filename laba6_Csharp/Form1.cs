3be15cc918f8c2f456a581e0698ffafc9fe7470a
Anna <hanna.hanna.2020@list.ru>
2025-05-06 12:44:22 +0800

    Добавьте файлы проекта.

---
+++ laba6_Csharp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static laba6_Csharp.Particle;
+
+namespace laba6_Csharp
+{
+    public partial class Form1: Form
+    {
+        Emitter emitter = new Emitter(); // добавили эмиттер
+
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+        }
+
+        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+
+        // ну и обработка тика таймера, тут просто декомпозицию выполнили
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g); // а тут теперь рендерим через эмиттер
+            }
+
+            picDisplay.Invalidate();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // добавляем переменные для хранения положения мыши
+        //private int MousePositionX = 0;
+        //private int MousePositionY = 0;
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // а тут в эмиттер передаем положение мыфки
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
+        }
+    }
+}
